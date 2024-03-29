9c4b2d7dfcde55048f65a02319bdcd70dbf137e8
pxg0709 <Pgarhwal@hexure.com>
2024-03-29 16:18:28 +0530

    update the code for FBW for all the three modules

--- src/main/java/com/hexure/firelight/pages/LoginPage.java
+++ src/main/java/com/hexure/firelight/pages/LoginPage.java
@@ -9,13 +9,13 @@ import org.openqa.selenium.support.PageFactory;
 
 @Data
 public class LoginPage extends FLUtilities {
-    @FindBy(xpath = "//input[contains(@name,'UserName')]")
+    @FindBy(xpath = "//input[@id='userName' or contains(@name,'userName')]")
     private WebElement txtbox_userName;
 
-    @FindBy(xpath = "//input[contains(@name,'Password')]")
+    @FindBy(xpath = "//input[@id='password' or contains(@name,'password')]")
     private WebElement txtbox_Password;
 
-    @FindBy(xpath = "//input[contains(@id,'Login')]")
+    @FindBy(xpath = "//button[@id='login']")
     private WebElement btn_SignIn;
 
     public LoginPage(WebDriver driver) {
