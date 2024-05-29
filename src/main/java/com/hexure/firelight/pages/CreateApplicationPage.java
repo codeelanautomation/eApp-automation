3f067326b8c7da951166a6221a5b3f723dfe353c
gxk0124 <gkhandelwal3@gmail.com>
2024-05-29 15:58:52 +0530

    add wait 2 sec.

--- src/main/java/com/hexure/firelight/pages/CreateApplicationPage.java
+++ src/main/java/com/hexure/firelight/pages/CreateApplicationPage.java
@@ -68,6 +68,7 @@ public class CreateApplicationPage extends FLUtilities {
         waitForPageToLoad(driver);
         sleepInMilliSeconds(2000);
         new Select(getDdProductType()).selectByVisibleText(productType);
+        sleepInMilliSeconds(2000);
         clickElement(driver, findElement(driver, String.format(listOfProducts, product)));
         waitForPageToLoad(driver);
         clickElement(driver, getBtnCreate());
