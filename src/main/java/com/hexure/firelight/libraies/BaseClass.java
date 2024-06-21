ba97269f089c633eb55cb34cb5c2746f3947a75a
gxk0124 <gkhandelwal3@gmail.com>
2024-06-21 11:06:01 +0530

    remove println

--- src/main/java/com/hexure/firelight/libraies/BaseClass.java
+++ src/main/java/com/hexure/firelight/libraies/BaseClass.java
@@ -463,7 +463,6 @@ public class BaseClass {
         wait.until(webdriver -> {
             Long loaderCount = (Long) ((JavascriptExecutor) driver).executeScript(
                     "return document.getElementsByClassName('" + loaderClassName + "').length;");
-            System.out.println("The loader Count is " + loaderCount);
             return loaderCount == 0;
         });
     }
