1df0c47eab9f56027f15fd43c5a74e9b9a5354fb
cxs1028 <cxs1028@insurancetechnologies.com>
2024-06-24 18:55:21 +0530

    update to delete temp file code

--- src/test/java/com/hexure/firelight/runner/RunInterfaceTest.java
+++ src/test/java/com/hexure/firelight/runner/RunInterfaceTest.java
@@ -7,12 +7,10 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -37,33 +35,34 @@ public class RunInterfaceTest {
     public static void tempDelete() {
         try {
             Path tempDir = Paths.get(System.getProperty("java.io.tmpdir"));
-            deleteDirectoryContents(tempDir);
-            System.out.println("All files in the temp directory have been deleted.");
+            AtomicInteger totalDeletedCount = new AtomicInteger();
+
+            Files.walk(tempDir)
+                    .parallel() // Process files in parallel
+                    .forEach(path -> {
+                        try {
+                            deleteFile(path);
+                            totalDeletedCount.incrementAndGet();
+                        } catch (IOException e) {
+//                                System.err.println("Failed to delete " + path.toString() + ": " + e.getMessage());
+                        }
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private static void deleteDirectoryContents(Path dir) throws IOException {
-        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                attemptToDeleteFile(file);
-                return FileVisitResult.CONTINUE;
-            }
-            @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                attemptToDeleteFile(dir); // Delete directory after its contents are deleted
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
-    private static void attemptToDeleteFile(Path filePath) throws IOException {
-        int maxRetries = 1;
+
+    private static void deleteFile(Path path) throws IOException {
+        int maxRetries = 3;
         for (int retry = 0; retry < maxRetries; retry++) {
             try {
-                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(path);
                 return;
             } catch (IOException e) {
+                // Log the failure, or you can retry
+                if (retry == maxRetries - 1) {
+                    throw e; // Throw exception if max retries reached
+                }
             }
         }
     }
